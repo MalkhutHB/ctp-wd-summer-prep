d52eb9d947529fc7babb42bea2a7536d62237fe9
malkhut <183023495+MalkhutHB@users.noreply.github.com>
2025-08-13 16:48:44 -0400

    add streak calculation

--- habit-tracker/C11/MalkhutHB/main.js
+++ habit-tracker/C11/MalkhutHB/main.js
@@ -148,10 +148,6 @@ function renderDaysEmpty() {
     }
 }
 
-function getDayButton(day) {
-    
-}
-
 function extenderClear() {
     inputTitle.value = "";
     inputTime.value = "10:00";  //
@@ -185,7 +181,7 @@ function habitInteract(e, article, habitId) {
             habits[habitId].completionDates.push(new Date());
             localStorage.setItem("habits", JSON.stringify(habits));
         }
-    } else if (e.target == percentInner) { //console.log("inner pressed");
+    } else if (e.target == percentInner) { // don't want to expand because this element has a doubleclick action
     } else if (repeatOptions.contains(e.target)) {
         if (e.target.classList.contains("daily")) {
             habits[habitId].repeat = "daily";
@@ -253,7 +249,7 @@ function renderHabits(section) {
         const completionDates = habits[habitId].completionDates;
         const repeatDays = habits[habitId].repeatDays;
         const oneDayInMs = 1000 * 60 * 60 * 24;
-        const today = new Date(calendarDay); /*marker for later*/ /*will make calandarDate() for fake dates*/ /* Wait no. I just need to pass a var to date... And change on calandar click what am I saying*/
+        const today = new Date(calendarDay); /*marker for later*/ 
 
         article.dataset.habitId = habitId;  
         if (completionDates && isSameTimeframe(timeframe, repeatDays, today, completionDates.at(-1))) {
@@ -353,13 +349,35 @@ function isSameTimeframe(repeat, repeatDays, date1, lastCompletedDate) {
     }
 }
 
-function calcLastSuchDay(day) {
-    const today = new Date(calendarDay); /*marker for later*/
+function isSameTimeframe2(repeat, repeatDays, date1, lastCompletedDate) {
+    if (!lastCompletedDate) return false;
+
+    date1.setHours(0, 0, 0, 0);
+    lastCompletedDate.setHours(0, 0, 0, 0);
+    if (repeat === "daily") {
+        return date1.getTime() === lastCompletedDate.getTime();
+    }
+    else if (repeat === "weekly") {
+        for (const weekday of repeatDays) {
+            let lastSuchDay = calcLastSuchDay(weekday, date1);
+            lastSuchDay.setDate(lastSuchDay.getDate() - 1);
+            lastSuchDay = calcLastSuchDay(weekday, lastSuchDay);
+            if (lastSuchDay > lastCompletedDate) return false;
+        }
+        return true;
+    }
+    else if (repeat === "none") {
+        return true; 
+    }
+}
+
+function calcLastSuchDay(day, date1) {
+    const today = new Date(date1 ?? calendarDay); /*marker for later*/
     const lastDayNumber = daysArray.indexOf(day);
     const todayNumber = today.getDay();
     let diff = todayNumber - lastDayNumber;
     if (diff < 0) diff += 7;
-    const lastSuch = new Date(calendarDay); /*marker for later*/
+    const lastSuch = new Date(date1 ?? calendarDay); /*marker for later*/
     lastSuch.setDate(lastSuch.getDate() - diff);
     lastSuch.setHours(0,0,0,0);
     return lastSuch;
@@ -368,20 +386,25 @@ function calcLastSuchDay(day) {
 function getStreakString(habitId) {
     const completionDates = habits[habitId].completionDates;
     const length = completionDates.length;
-    if (length) return `🌀${completionDates.length}`;
+
+    const timeframe = habits[habitId].repeat;    
+    const repeatDays = habits[habitId].repeatDays;
+
+    let recentStreak = 1; // if not sametimeframe outside of loop, dont return recent return 0
+
+    for (let i=length-1; i>0; i--) {
+        if (isSameTimeframe2(timeframe, repeatDays, completionDates[i], completionDates[i-1])) {
+            recentStreak++;
+        } else {
+            break;
+        }
+    }
+
+    if (length > 1 && isSameTimeframe2(timeframe, repeatDays, completionDates.at(-1), completionDates.at(-2))) return `🌀${recentStreak}`;
+    else if (length >= 1 && isSameTimeframe2(timeframe, repeatDays, completionDates.at(-1), TODAY)) return `🌀${recentStreak}`;
     else return ``;
 }
 
-// function to calc streak. Basically did alr. But, need local storage for this to even matter. 
-// Can test by adding parameter for date and inputting fake dates. Actually yeah we should do this before local storage implementation. 
-
-// wait no, streaks would be from markcomplete. So need an array with dates associated to a habit. 
-// Prob can hash habits by exact start time
-
-// markcomplete:
-// Shouldn't have the opportunity to mark it complete until it reopens. I guess to make it simple, at 00:00 the day, and the next day/week
-// Need to add frequency paramater to addHabit for this. 
-// And again for testing, add date parameter to completeHabit
 
 
 // today - habits[habitId].completionDates.at(-1) < oneDayInMs)
